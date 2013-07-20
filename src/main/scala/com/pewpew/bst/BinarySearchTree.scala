537a5e8611220ae22a6827cb5faec5729cbf5b07
Piyush Srivastava <piyush.0101@gmail.com>
2013-07-20 17:28:11 -0500

    adding a mirror function

--- src/main/scala/com/pewpew/bst/BinarySearchTree.scala
+++ src/main/scala/com/pewpew/bst/BinarySearchTree.scala
@@ -1,47 +1,57 @@
 import com.pewpew.bst._
 
-def insert(element: Int, tree: BinaryTree): BinaryTree = {
-  tree match {
-    case EmptyTree() => Node(element, EmptyTree(), EmptyTree())
-    case Node(elem, left, right) if element < elem => Node(elem, insert(element, left), right)
-    case Node(elem, left, right) if element > elem => Node(elem, left, insert(element, right))
-    case Node(elem, left, right) if element == elem => Node(elem, left, right)
-  }
-}
+class BinarySearchTree {
 
-def fill(elements: List[Int], tree: BinaryTree): BinaryTree = {
-  elements match {
-    case x :: xs => fill(xs, insert(x, tree))
-    case nil => tree
+  def insert(element: Int, tree: BinaryTree): BinaryTree = {
+    tree match {
+      case EmptyTree() => Node(element, EmptyTree(), EmptyTree())
+      case Node(elem, left, right) if element < elem => Node(elem, insert(element, left), right)
+      case Node(elem, left, right) if element > elem => Node(elem, left, insert(element, right))
+      case Node(elem, left, right) if element == elem => Node(elem, left, right)
+    }
   }
-}
 
-def preOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
-  tree match {
-    case EmptyTree() => List()
-    case Node(elem, left, right) => traversal ::: List(elem) ::: preOrder(left, traversal) ::: preOrder(right, traversal)
+  def fill(elements: List[Int], tree: BinaryTree): BinaryTree = {
+    elements match {
+      case x :: xs => fill(xs, insert(x, tree))
+      case nil => tree
+    }
   }
-}
 
-def postOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
-  tree match {
-    case EmptyTree() => List()
-    case Node(elem, left, right) => traversal ::: postOrder(left, traversal) ::: postOrder(right, traversal) ::: List(elem)
+  def preOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
+    tree match {
+      case EmptyTree() => List()
+      case Node(elem, left, right) => traversal ::: List(elem) ::: preOrder(left, traversal) ::: preOrder(right, traversal)
+    }
   }
-}
 
-def inOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
-  tree match {
-    case EmptyTree() => List()
-    case Node(elem, left, right) => traversal ::: inOrder(left, traversal) ::: List(elem) ::: inOrder(right, traversal)
+  def postOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
+    tree match {
+      case EmptyTree() => List()
+      case Node(elem, left, right) => traversal ::: postOrder(left, traversal) ::: postOrder(right, traversal) ::: List(elem)
+    }
   }
-}
 
-def find(tree: BinaryTree, x: Int): Option[Int] = {
-  tree match {
-    case EmptyTree() => None
-    case Node(elem, left, right) if (x == elem) => Some(x)
-    case Node(elem, left, right) if (x < elem) => find(left, elem)
-    case Node(elem, left, right) if (x > elem) => find(right, elem)
+  def inOrder(tree: BinaryTree, traversal: List[Int]): List[Int] = {
+    tree match {
+      case EmptyTree() => List()
+      case Node(elem, left, right) => traversal ::: inOrder(left, traversal) ::: List(elem) ::: inOrder(right, traversal)
+    }
+  }
+
+  def find(tree: BinaryTree, x: Int): Option[Int] = {
+    tree match {
+      case EmptyTree() => None
+      case Node(elem, left, right) if (x == elem) => Some(x)
+      case Node(elem, left, right) if (x < elem) => find(left, elem)
+      case Node(elem, left, right) if (x > elem) => find(right, elem)
+    }
+  }
+
+  def mirror(tree: BinaryTree) : BinaryTree = {
+    tree match {
+      case EmptyTree() => EmptyTree()
+      case Node(elem, left, right) => Node(elem, mirror(right), mirror(left))
+    }
   }
 }
